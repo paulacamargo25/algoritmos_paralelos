55953a75cb7a186290240243793fa984747f5b21
paulacamargo25 <paula@inka-labs.com>
2017-05-25 16:28:25 -0500

    Ejercicios cuda

---
+++ CUDA/MxV.cu
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+__global__
+void matrixVectorKernel(float* A, float* B, float* C, int n)
+{
+	int i = threadIdx.x + (blockDim.x * blockIdx.x);
+	if(i<n)
+	{
+		C[i] = 0;
+		for(int j=0;j<n;j++)
+			 C[i] += A[i*n+j] * B[j];
+
+	}
+	
+}
+
+
+
+void matrixVector(float* A, float* B, float* C, int tam)
+{
+	int sizeA = (tam*tam) * sizeof(float);
+	int size =  tam * sizeof(float);
+	float *d_A,*d_B,*d_C;
+
+	cudaMalloc((void**)&d_A,sizeA);
+	cudaMalloc((void**)&d_B,size);
+	cudaMalloc((void**)&d_C,size);
+
+	cudaMemcpy(d_A,A,sizeA,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+
+	matrixVectorKernel<<<ceil(tam/256.0),256>>>(d_A,d_B,d_C,tam);
+	cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+
+	cudaFree(d_A);cudaFree(d_B);cudaFree(d_C);
+	
+}
+
+int main()
+{
+	int n = 100;
+	float *h_A,*h_B,*h_C;
+	h_A = new float[n*n];
+	h_B = new float[n*n];
+	h_C = new float[n*n];
+
+	for(int i = 0; i < n; i++)
+	{
+	   for(int j = 0; j < n; j++)
+	     h_A[i*n+j] = rand() % n;
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+	    h_B[i] = rand() % n;
+	}
+
+	matrixVector(h_A,h_B,h_C,n);
+	for(int i = 0; i < n; i++){
+		cout<<h_C[i]<<" ; ";
+	}
+
+	cout<<endl;
+	return 0;
+}
